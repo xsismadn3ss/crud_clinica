df52920a9d8f2eae7e82ff5482fe2580229c6abc
HAROLD ARMANDO HERRERA ALVAREZ <2023010484@cvirtualuees.edu.sv>
2023-11-09 18:46:17 -0600

    Login

---
+++ WEB¿CLINICA/Areas/Identity/Data/WEB_CLINICAContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WEB_CLINICA.Data;
+
+public class WEB_CLINICAContext : IdentityDbContext<IdentityUser>
+{
+    public WEB_CLINICAContext(DbContextOptions<WEB_CLINICAContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
